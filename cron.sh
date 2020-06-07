c98245cd61580cb2b684efcd43b331e7fa424c3c
Anadian <willanad@yandex.com>
2020-06-07 14:07:13 -0400

    chore: Prosperity commit before cleaning things up.

--- cron.sh
+++ cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'function-factory/cron.sh';
 date -uI'seconds';
-git checkout master;
+git checkout wip;
 git add --all .;
 if git commit -m 'Cron.sh auto commit.' ; then
 	git push origin master;
