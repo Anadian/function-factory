46dec9d3ae2016fc5c83de3360612cd5bfb4dec3
Anadian <willanad@yandex.com>
2021-02-21 22:11:36 -0500

    chore: Cleaning

--- scripts/copy-static-dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp -f ~/dev/js-sandbox/application-log-standard.js source/application-log-standard.js
-cp -f ~/dev/jsonic-parse/source/main.js source/jsonic-parse.js
--- scripts/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ln -sf ${PWD}/function-factory.js /usr/local/bin/function-factory
-ln -sf ${PWD}/Resources /Users/cameron/Library/Application\ Support/function-factory-nodejs
